e65a6fdc874fdb3f89f30168dfd8b35d4c27cdcb
imleticio <mleonel.martinez0098@gmail.com>
2025-09-19 02:55:25 -0300

    fix/client

--- server/src/depositos/dto/update-deposito.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDepositoDto } from './create-deposito.dto';
-
-export class UpdateDepositoDto extends PartialType(CreateDepositoDto) {}
